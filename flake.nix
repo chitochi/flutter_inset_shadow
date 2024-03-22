960f2732d866194da7f389b3b1eba59f4f316ab3
Chito <chitochi@proton.me>
2024-03-22 14:20:45 +0100

    Add a simple Nix Flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development environment for flutter_inset_shadow.";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            flutter
+          ];
+        };
+      }
+    );
+}
